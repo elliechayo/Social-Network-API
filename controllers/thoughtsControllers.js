9da0a0e7defeab72a1f749a1d2ec21eb0212f387
Elinor Chayo <elliechayo@gmail.com>
2023-06-02 09:46:44 -0400

    added the functions to: 'modify/update thought by id', 'fetch and return all thoughts' and 'fetch a thought by id' to teh thoughtscontrollers file

--- controllers/thoughtsControllers.js
+++ controllers/thoughtsControllers.js
@@ -2,6 +2,31 @@ const Thought = require("../models/Thought");
 const User = require("../models/User");
 
 
+// fetch all thoughts and return it
+const getThoughts = async (req, res) => {
+     try {
+       const allThoughts = await Thought.find({}).populate("reactions");
+       return res.json(allThoughts);
+     } catch (error) {
+       return res.status(500).json({ error: error.message });
+     }
+   };
+   
+   // fetch and return a single thought based on the given id
+   const getSingleThought = async (req, res) => {
+     try {
+       const singleThought = await Thought.findOne({
+         _id: req.params.id,
+       }).populate("reactions");
+       if (!singleThought) {
+         return res.status(404).json({ error: "No thoughts found with that id" });
+       }
+       return res.json(singleThought);
+     } catch (error) {
+       return res.status(500).json({ error: error.message });
+     }
+   };
+
 // create a new thought
 // add the newly created thought to the given user
 const createThought = async (req, res) => {
@@ -25,4 +50,22 @@ const createThought = async (req, res) => {
        return res.status(500).json({ error: error.message });
      }
    };
+   // update a thought based on the id
+const updateThought = async (req, res) => {
+     try {
+       const updatedThought = await Thought.findOneAndUpdate(
+         { _id: req.params.id },
+         req.body,
+         { new: true, runValidators: true }
+       );
+   
+       if (!updatedThought) {
+         return res.status(404).json({ error: "No thought found with that id" });
+       }
+       res.json(updatedThought);
+     } catch (error) {
+       return res.status(500).json({ error: error.message });
+     }
+   };
+
    
